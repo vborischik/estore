4fee9552756199f672db7604c3c459221b1d4ca8
Vlad <mail@mail.com>
2025-04-14 21:16:47 -0500

    fixing bugs

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://gf8r34ks26.execute-api.us-east-1.amazonaws.com/prod/api'
+  };
--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,10 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://gf8r34ks26.execute-api.us-east-1.amazonaws.com/prod/api'
+};
+
+
+
 // export const environment = {
 //     production: false,
-//     apiUrl: 'http://estoreweb-test.us-east-1.elasticbeanstalk.com/api'
+//     apiUrl: 'http://localhost:5033/api'
 //   };
 
-
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5033/api'
-  };
